a9ae9939d1a191f2236bfececc53a130bdf05610
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-06-09 17:02:56 -0300

    cool

---
+++ modulo1/revisao.js
@@ -0,0 +1,33 @@
+// web e apis
+/*
+npm é o node project manager ele serve para instalar pacotes externos 
+npm i <dependencia> - instala uma dependencia ou se nenhuma dependencia for específicada atualiza as dependencias já instaladas do projeto
+npm uninstall <dependencia> - desinstalla uma dependencia
+npm init - inicia uma estrutura básica de um projeto npm
+
+no package.json é possível especificar scripts que podem ser chamados com o comando npm como "npm start" 
+*/
+import express from 'express';
+// apis são aplicações web backend que devolvem informações ou executam ações baseado em requests http
+const app = express();
+const port = 3000;
+
+//define uma rota que ativa uma função
+app.get('/users', (req, res) => {
+    //
+    try {
+        // faz alguma coisa
+        // manda informação
+        res.status(200).json({"answer": "cool", "users": "data"});
+    }
+    catch (err) {
+        // pega o erro de execução de codigo e trata ele
+        res.status(500).json({"error": "bro this shit not work man what the hell"})
+    }
+})
+
+// faz o codigo escutar na porta port (3000)
+app.listen(port, () => {
+    //faz alguma coisa quando o código começa a rodar
+    console.log("yo bro its running hahhahahahahh");
+});
---
+++ modulo2/revisao.js
@@ -0,0 +1,13 @@
+//git
+/*
+git add <arquivo> - faz o git registrar um arquivo como sendo do projeto
+git rm <arquivo> - faz o contrário
+git commit - dá commit no código e assim esse estado do projeto fica registrado
+git push - salva o código no repositório remoto (normalmente o github)
+git pull - puxa o código salvo no repositório remoto 
+git init - inicia o git no repositório
+.gitignore - ignora alguns arquivos ou pastas 
+
+
+
+*/
