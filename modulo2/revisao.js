3729d05a6120158e6606a1826cca7c400e629e39
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-06-09 17:37:39 -0300

    cool

--- modulo2/revisao.js
+++ modulo2/revisao.js
@@ -8,6 +8,21 @@ git pull - puxa o código salvo no repositório remoto
 git init - inicia o git no repositório
 .gitignore - ignora alguns arquivos ou pastas 
 
+nodemon - forma de rodar o node que faz o node resetar a cada mudança de código. Bom para desenvolvimento
+modulo http ajuda a servers http de forma nativa a o node js mas é obsoleto
+modulo fs (file system) permite manipular arquivos usando o node
 
 
+insomnia - software q permite testar requests
 */
+
+import http from 'http';
+import fs from 'fs';
+
+http.createServer(function (req, res) {
+    fs.readFile('demofile1.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(3000);
